41fc19dd93e0632818947ccc14417ed3ea02d2f9
PiWalker <ldspcjunkie@gmail.com>
2015-07-23 16:49:27 -0600

    Mod Setup.

---
+++ src/main/java/com/piwalker/emeraldtools/EmeraldTools.java
@@ -0,0 +1,15 @@
+package com.piwalker.emeraldtools;
+
+import com.piwalker.emeraldtools.references.References;
+import cpw.mods.fml.common.Mod;
+
+/**
+ * Created by SamuelPiWalker on 7/23/2015.
+ */
+@Mod(modid = References.MOD_ID, name = References.MOD_NAME, version = References.MOD_VERSION)
+public class EmeraldTools {
+
+    @Mod.Instance(References.MOD_ID)
+    public static EmeraldTools instance;
+
+}
---
+++ src/main/java/com/piwalker/emeraldtools/references/References.java
@@ -0,0 +1,12 @@
+package com.piwalker.emeraldtools.references;
+
+/**
+ * Created by SamuelPiWalker on 7/23/2015.
+ */
+public class References {
+    public static final String MOD_ID = "emeraldtools";
+    public static final String MOD_NAME = "Emerald Tools";
+    public static final String MOD_VERSION = "1.7.10-1.0";
+    public static final String CLIENT_PROXY_CLASS = "com.piwalker.emeraldtools.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.piwalker.emeraldtools.proxy.ServerProxy";
+}
